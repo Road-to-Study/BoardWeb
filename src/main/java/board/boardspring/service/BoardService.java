6ba7cacefb27478947326b601be27314b452341c
YOOBINNOH <kuninyb@naver.com>
2022-08-31 14:26:26 +0900

    feat 테스트 코드 작성 완료

--- src/main/java/board/boardspring/service/BoardService.java
+++ src/main/java/board/boardspring/service/BoardService.java
@@ -32,7 +32,7 @@ public class BoardService {
     // 특정 게시물 불러오기
     public Board boardView(Integer id){
 
-        return boardRepository.findById(id).get();
+        return boardRepository.findById(id).orElse(null);
     }
 
     // 특정 게시글 삭제
@@ -43,9 +43,16 @@ public class BoardService {
     }
 
 
+    // 검색 기능
     public Page<Board> search(String searchKeyword, Pageable pageable) {
 
         return boardRepository.findByTitleContaining(searchKeyword, pageable);
     }
+
+
+
+
+
+
 
 }
--- src/test/java/board/boardspring/BoardSpringApplicationTests.java
+++ src/test/java/board/boardspring/BoardSpringApplicationTests.java
@@ -1,13 +1,70 @@
 package board.boardspring;
 
+import board.boardspring.controller.BoardController;
+import board.boardspring.domain.Board;
+import board.boardspring.repository.BoardRepository;
+import board.boardspring.service.BoardService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @SpringBootTest
 class BoardSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+
+	@Autowired
+	BoardService boardService;
+	BoardRepository boardRepository;
+
+
+
+//	@Test
+//	@DisplayName("Board 생성 테스트")
+//	public void ID_제목_내용() {
+//
+//		Board board = new Board();
+//
+//		board.setId(1);
+//		board.setTitle("test title");
+//		board.setContent("test content");
+//
+//
+//		Assertions.assertEquals(1, board.getId());
+//		Assertions.assertEquals("test title", board.getTitle());
+//		Assertions.assertEquals("test content", board.getContent());
+//
+//	}
+
+
+//	@Test
+//	@DisplayName("글 작성 테스트")
+//	public void 글_작성() {
+//
+//		Board board = new Board();
+//
+//
+//
+//		board.setTitle("test title");
+//		board.setContent("test content");
+//
+//		boardService.write(board);
+//
+//		boardService.boardDelete(15);
+//
+//	}
+
 
 }
