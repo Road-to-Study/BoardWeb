e63efb10334c5325d2166ad4afc99955eea5290f
YOOBINNOH <kuninyb@naver.com>
2022-09-05 23:07:49 +0900

    feat 로그인 진행 중

--- src/main/java/board/boardspring/controller/MemberController.java
+++ src/main/java/board/boardspring/controller/MemberController.java
@@ -1,70 +1,69 @@
 package board.boardspring.controller;
 
-<<<<<<< HEAD
+
 import board.boardspring.domain.Board;
-=======
+
 import board.boardspring.domain.Member;
+import board.boardspring.repository.MemberRepository;
 import board.boardspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 4f5a33498283ccf9bf47f76b09be9bd21933edac
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class MemberController {
 
-<<<<<<< HEAD
+    @Autowired
+
+
     @GetMapping("/login")
     public String login() {
 
         return "login";
     }
 
-//    @PostMapping("/login")
-//    public String login() {
-//
-//        return "login";
-//    }
-//
+    // Login 구현
+    @PostMapping("/login")
+    public String logincheck(HttpServletRequest httpServletRequest) {
+        String ID = httpServletRequest.getParameter("login_id");
+        String PWD = httpServletRequest.getParameter("login_pwd");
+
+        boolean success = memberService.logincheck(ID,PWD);
+
+
+
+        return "login";
+    }
+
+
+
+    @Autowired
+    private MemberService memberService;
 
     @GetMapping("/register")
-    public String register() {
+    public String signup() {
 
         return "register";
     }
 
-//    @PostMapping("/register")
-//    public String register() {
-//
-//        return "register";
-//    }
-
-=======
-    @Autowired
-    private MemberService memberService;
-
-    @GetMapping("/signup")
-    public String signup() {
-
-        return "signup";
-    }
-
-    @PostMapping("/signup")
+    @PostMapping("/register")
     public String signupPost(Member member) {
-        if(memberService.validateDuplicate(member.getId())) {
-            return "signup";
+        if (memberService.validateDuplicate(member.getId())) {
+            return "register";
         }
-        if(!memberService.checkIdForm(member.getId())) {
-            return "signup";
+        if (!memberService.checkIdForm(member.getId())) {
+            return "register";
         }
-        if(!memberService.checkPasswordForm(member.getPassword())) {
-            return "signup";
+        if (!memberService.checkPasswordForm(member.getPassword())) {
+            return "register";
         }
 
         memberService.join(member);
 
         return "redirect:/";
     }
->>>>>>> 4f5a33498283ccf9bf47f76b09be9bd21933edac
 }
--- src/main/java/board/boardspring/service/MemberService.java
+++ src/main/java/board/boardspring/service/MemberService.java
@@ -5,6 +5,8 @@ import board.boardspring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @Service
@@ -17,17 +19,41 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    // 아이디 중복 체크
+
     public boolean validateDuplicate(String id) {
         return memberRepository.findById(id).isPresent();
     }
+
+    // 아이디 형식 체크
 
     public boolean checkIdForm(String id) {
         String pattern = "^[a-zA-Z0-9]{6,20}$";
         return Pattern.matches(pattern, id);
     }
 
+    // 비밀번호 형식 체크
+
     public boolean checkPasswordForm(String password) {
         String pattern = "^[a-zA-Z0-9!@#$%^&*]{6,20}$";
         return Pattern.matches(pattern, password);
     }
+
+    // 로그인 성공 체크
+
+    public boolean logincheck(String ID, String PWD){
+
+        boolean success = false;
+
+        if(memberRepository.findById(ID).isPresent()){
+
+
+        }
+
+
+
+        return success;
+    }
+
+
 }
