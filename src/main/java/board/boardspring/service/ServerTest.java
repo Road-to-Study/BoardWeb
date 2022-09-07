7d371951f92f79441026c46f59916fee13e660f5
YOOBINNOH <kuninyb@naver.com>
2022-09-07 10:57:00 +0900

    feat 서버 테스트 성공

---
+++ src/main/java/board/boardspring/service/ServerTest.java
@@ -0,0 +1,16 @@
+package board.boardspring.service;
+
+public class ServerTest {
+
+//1. ArtilleryVisit Website 이용
+
+// npm i -D artillery
+// npm start
+// npx artillery quick --count 100 -n 50 http://localhost:8080/write
+
+//2. JMeter 사용
+
+// cd bin 하고 ./jmeter
+
+
+}
